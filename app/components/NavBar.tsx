0d6a4db0001cbcb5ec2a80335d81f4fe11a5aa75
Franco Poffo <franco_sutter@yahoo.com.br>
2023-06-13 12:30:41 -0300

    Limpando os arquivos iniciais do Next, criando variáveis globais das cores, e criação do Header desktop

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image"
+import Link from "next/link"
+
+
+const NavBar = () => {
+  return (
+    <header className="flex items-center justify-between mt-20 mb-16">
+        <Link href="/">
+        <Image src="/images/logo.svg" alt="Logo" width={75} height={75}/>
+        </Link>
+        <nav>
+            <ul className="flex gap-12">
+                <li><Link href="/">Home</Link></li>
+                <li><Link href="#">New</Link></li>
+                <li><Link href="#">Popular</Link></li>
+                <li><Link href="#">Trending</Link></li>
+                <li><Link href="#">Categories</Link></li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default NavBar;
